802b2a060d6c1f2c2fd4da361dd307f7355bd239
Ian Turner <ijturner.me@gmail.com>
2022-12-17 21:56:03 +0000

    Remove authentication references from guest layout

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -4,6 +4,12 @@ import GuestLayout from '@/Layouts/GuestLayout.vue'
 
 <template>
   <GuestLayout>
-    Hello
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          Hello
+        </div>
+      </div>
+    </div>
   </GuestLayout>
 </template>
