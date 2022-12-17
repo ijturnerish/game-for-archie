62b56f0684f9d914eb10c9a49fb908c904155fd1
Ian Turner <62931405+ijturnerish@users.noreply.github.com>
2022-12-17 21:59:01 +0000

    Feature/simplify layout (#3)
    * Remove welcome view as we won't be using it
    
    * Add empty home page
    
    * Add (broken) guest layout
    
    * Remove authentication references from guest layout

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3'
+import ApplicationMark from '@/Components/ApplicationMark.vue'
+import Banner from '@/Components/Banner.vue'
+
+defineProps({
+  title: String,
+})
+</script>
+
+<template>
+  <div>
+
+    <Head :title="title" />
+
+    <Banner />
+
+    <div class="min-h-screen bg-gray-100">
+      <nav class="bg-white border-b border-gray-100">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="flex justify-between h-16">
+            <div class="flex">
+              <div class="shrink-0 flex items-center">
+                <Link :href="route('dashboard')">
+                <ApplicationMark class="block h-9 w-auto" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <header v-if="$slots.header" class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <slot name="header" />
+        </div>
+      </header>
+
+      <main>
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+</script>
+
+<template>
+  <GuestLayout>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          Hello
+        </div>
+      </div>
+    </div>
+  </GuestLayout>
+</template>
